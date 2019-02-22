f06a4f6810962aa32f175c53a20fc129bd705ea0
Tom Chapman <chapman@live.com.au>
2019-02-22 15:20:33 +1000

    .

--- src/views/Credits/index.js
+++ src/views/Credits/index.js
@@ -94,10 +94,29 @@ class Credits extends React.Component {
     );
   }
 
+  shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+
   async componentDidMount() {
     window.scrollTo(0, 0);
 
-    let memberResponses = await this.getMembers(this.supporters);
+    let memberResponses = await this.getMembers(this.shuffle(this.supporters));
 
     this.setState(p => {
       p.supporters = memberResponses;
