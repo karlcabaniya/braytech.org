125e9989c61139749cc100592d89ff08e40c0fc5
Tom Chapman <chapman@live.com.au>
2019-09-02 09:57:47 +1000

    Update index.js

--- src/components/Tooltip/Activity/index.js
+++ src/components/Tooltip/Activity/index.js
@@ -21,32 +21,6 @@ class Activity extends React.Component {
       return null;
     }
 
-    let lol = manifest.DestinyMilestoneDefinition[1300394968].activities.concat([{activityHash:2307090074}, {activityHash:2067233851}, {activityHash:185515551}, {activityHash:1063969232}, {activityHash:1773400654}]).map(m => {
-      return manifest.DestinyActivityDefinition[m.activityHash].displayProperties.name;
-    }).map(n => n.replace('(Heroic)','').trim());
-
-    let lol2 = manifest.DestinyMilestoneDefinition[1300394968].activities.concat([{activityHash:2307090074}, {activityHash:2067233851}, {activityHash:185515551}, {activityHash:1063969232}, {activityHash:1773400654}]).map(m => {
-      return m.activityHash;
-    });
-
-    let lol3 = lol2.filter(function(elem, index, self) {
-      return index == self.indexOf(elem);
-    });
-
-    let lol4 = Object.values(manifest.DestinyActivityDefinition).filter(m => m.displayProperties && m.displayProperties.name && lol.includes(m.displayProperties.name.replace('(Heroic)','').trim()))
-
-    let lol5 = lol4.map(m => {
-      return m.hash;
-    }).concat(lol2);
-
-    let lol6 = lol5.filter(function(elem, index, self) {
-      return index == self.indexOf(elem);
-    });
-
-    console.log(JSON.stringify(lol6), lol4.map(d => d.displayProperties.name).map(n => n.replace('(Heroic)','').trim()).filter(function(elem, index, self) {
-      return index == self.indexOf(elem);
-    }))
-
     if (definitionActivity.redacted) {
       return (
         <>
@@ -68,8 +42,10 @@ class Activity extends React.Component {
       );
     } else {
 
-      const activityType = (activityModeHashes = []) => {
-        if (activityModeHashes.includes(1686739444)) {
+      const activityType = (hash, activityModeHashes = []) => {
+        if ([53954174, 78673128, 96442917, 122988657, 185515551, 319240296, 320680002, 340004423, 359488722, 449926115, 539897061, 622895925, 625165976, 632790902, 723733266, 785871069, 789332628, 801458995, 808931822, 856342832, 919252154, 963938931, 993905880, 999972877, 1018040791, 1018385878, 1063969232, 1107208644, 1159314159, 1225970098, 1228327586, 1254990192, 1265390366, 1275562432, 1279862229, 1289867188, 1294490226, 1302437673, 1333621919, 1416597166, 1418217191, 1466550401, 1491022087, 1503376677, 1570598249, 1643069750, 1651979106, 1657356109, 1671235700, 1682036469, 1725302079, 1740310101, 1773400654, 1783922093, 1800749202, 1811228210, 1823921651, 1824067376, 1829866365, 1874578888, 1956541147, 1969800443, 1971154629, 1981289329, 1987624188, 2067233851, 2069143995, 2174556965, 2219006909, 2231840083, 2245202378, 2250935166, 2258680077, 2302677459, 2307090074, 2310677039, 2340776707, 2517540332, 2574607799, 2665134323, 2675435236, 2737739053, 2752743635, 2831644165, 2949941834, 2966841322, 3002511278, 3015346707, 3033151437, 3042112297, 3069330044, 3140524926, 3148431353, 3211568383, 3248193378, 3255524827, 3277510674, 3283790633, 3289681664, 3304835347, 3370527053, 3384410381, 3485876484, 3500791146, 3601218952, 3644215993, 3645117987, 3664729722, 3664915501, 3691789482, 3700722865, 3752039537, 3780356141, 3836086286, 3872525353, 3909841711, 3920569453, 4094398454, 4238309598].includes(hash)) {
+          return 'adventure';
+        } else if (activityModeHashes.includes(1686739444)) {
           return 'story';
         } else if (activityModeHashes.includes(2394616003)) {
           return 'strike';
@@ -80,7 +56,7 @@ class Activity extends React.Component {
         }
       }
 
-      const modeFiltered = activityType(definitionActivity.activityModeHashes.concat(mode));
+      const modeFiltered = activityType(definitionActivity.hash, definitionActivity.activityModeHashes.concat(mode));
 
       let activityTypeDisplay = {
         name: definitionActivity.selectionScreenDisplayProperties && definitionActivity.selectionScreenDisplayProperties.name ? definitionActivity.selectionScreenDisplayProperties.name : definitionActivity.displayProperties && definitionActivity.displayProperties.name ? definitionActivity.displayProperties.name : t('Unknown'),
@@ -147,6 +123,8 @@ class Activity extends React.Component {
       if (modeFiltered === 'adventure')
         activityTypeDisplay = {
           ...activityTypeDisplay,
+          mode: t('Adventure'),
+          className: 'adventure',
           icon: (
             <span className='destiny-adventure2'>
               <span className='path1' />
