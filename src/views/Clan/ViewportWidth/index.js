923f8af696a04a5306d24e18dd91b96b28c655c7
Tom Chapman <chapman@live.com.au>
2019-09-03 14:49:05 +1000

    .

--- src/views/Clan/ViewportWidth/index.js
+++ src/views/Clan/ViewportWidth/index.js
@@ -3,6 +3,8 @@ import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { withTranslation } from 'react-i18next';
 
+import ClanViewsLinks from '../ClanViewsLinks';
+
 import './styles.css';
 
 class ViewportWidth extends React.Component {
@@ -10,14 +12,17 @@ class ViewportWidth extends React.Component {
     const { t } = this.props;
 
     return (
-      <div className='module'>
-        <div className='properties'>
-          <div className='name'>{t('Clan admin')}</div>
-          <div className='description'>
-            <p>{t('Clan Admin mode is intended for use on larger displays. Please use a display with a viewport of atleast 1280px.')}</p>
+      <>
+        <ClanViewsLinks {...this.props} />
+        <div className='module'>
+          <div className='properties'>
+            <div className='name'>{t('Clan admin')}</div>
+            <div className='description'>
+              <p>{t('Clan Admin mode is intended for use on larger displays. Please use a display with a viewport of atleast 1280px.')}</p>
+            </div>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 }
