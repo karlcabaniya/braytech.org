aa8311e617784f55ae322eab00411cac768dc74f
Tom Chapman <chapman@live.com.au>
2019-09-12 01:16:24 +1000

    .

--- src/views/Maps/index.js
+++ src/views/Maps/index.js
@@ -145,6 +145,12 @@ class Maps extends React.Component {
 
     const bounds = [[0, 0], [mapHeight, mapWidth]];
 
+    const icon = L.icon({
+      iconUrl: `data:image/svg+xml,%3Csvg version='1.1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1024 1024'%3E%3Cpath fill='white' d='M754 538h270v34l-434 452h-124l-466-452v-34h302l226 228zM302 486h-302v-38l466-448h124l434 448v38h-270l-226-228zM645.993 511.923q0 49.027-34.9 83.927t-83.096 34.9-83.096-34.9-34.9-83.927 34.9-83.096 83.096-34.070 83.096 34.070 34.9 83.096z'%3E%3C/path%3E%3C/svg%3E%0A`,
+      iconSize: [24, 24],
+      className: 'ghost-scan'
+    })
+
     return (
       <div className='view' id='maps'>
         <div className='map'>
@@ -182,7 +188,7 @@ class Maps extends React.Component {
               const offsetX = markerOffsetX+(node.map.x ? node.map.x : 0);
               const offsetY = markerOffsetY+(node.map.y ? node.map.y : 0);
               
-              return <Marker key={node.itemHash} position={[offsetY, offsetX]} />
+              return <Marker key={node.itemHash} position={[offsetY, offsetX]} icon={icon} />
             })}
           </Map>
         </div>
