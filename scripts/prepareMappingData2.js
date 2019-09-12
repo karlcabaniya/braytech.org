8d60665e51dd86801c6058377bdbacde74d850e0
Tom Chapman <chapman@live.com.au>
2019-09-12 16:05:29 +1000

    .

--- scripts/prepareMappingData2.js
+++ scripts/prepareMappingData2.js
@@ -4,44 +4,52 @@ const fetch = require('node-fetch');
 
 const outputPath = 'src/data/lowlines/maps/index.json';
 
-function work(input) {
+async function work(input) {
   const output = {};
-  
-  output.destination = {
-    hash: input.data.map.destinationHash,
-    name: input.data.map.name,
-    description: input.data.map.description,
-    id: input.data.map.id
-  };
 
-  output.map = {
-    width: input.data.map.width,
-    height: input.data.map.height,
-    layers: input.data.map.layers,
-    bubbles: input.data.map.locations.map(location => ({
-      name: location.title,
-      id: location.id,
-      type: location.public ? 'region' : location.lostSector ? 'lost-sector' : 'suburb',
-      nodes: location.nodes.filter(node => ['title', 'fast-travel'].includes(node.type)).map(node => {
-        const n = {
-          id: node.id,
-          type: node.type,
-          x: node.x,
-          y: node.y
-        }
+  await Promise.all(input.data.map(async d => {
+    const destination = await fetch('https://lowlidev.com.au/destiny/api/v2/map/data/' + d.id).then(res => res.json());
 
-        // if (node.type === 'title') n.name = location.title
+    const temp = {};
 
-        return n;
-      })
-    }))
-  };
-  
+    temp.destination = {
+      hash: destination.data.map.destinationHash,
+      name: destination.data.map.name,
+      description: destination.data.map.description,
+      id: destination.data.map.id
+    };
+
+    temp.map = {
+      width: destination.data.map.width,
+      height: destination.data.map.height,
+      layers: destination.data.map.layers,
+      bubbles: destination.data.map.locations && destination.data.map.locations.map(location => ({
+        name: location.title,
+        id: location.id,
+        type: location.public ? 'region' : location.lostSector ? 'lost-sector' : 'suburb',
+        nodes: location.nodes
+          .filter(node => ['title', 'fast-travel'].includes(node.type))
+          .map(node => {
+            const n = {
+              id: node.id,
+              type: node.type,
+              x: node.x,
+              y: node.y
+            };
+
+            // if (node.type === 'title') n.name = location.title
+
+            return n;
+          })
+      }))
+    };
+
+    output[d.id] = temp;
+  }));
+
   fs.writeFileSync(outputPath, JSON.stringify(output));
-  
 }
 
-fetch('https://lowlidev.com.au/destiny/api/v2/map/data/io?locale=en')
-    .then(res => res.json())
-    .then(json => work(json));
-
+fetch('https://lowlidev.com.au/destiny/api/v2/map/data')
+  .then(res => res.json())
+  .then(json => work(json));
--- src/views/Maps/index.js
+++ src/views/Maps/index.js
@@ -4,163 +4,25 @@ import { connect } from 'react-redux';
 import { withTranslation } from 'react-i18next';
 import cx from 'classnames';
 
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import { Map, ImageOverlay, Marker } from 'react-leaflet';
-
-import manifest from '../../utils/manifest';
-import map from '../../data/lowlines/maps';
-
-import ChecklistFactory from '../Checklists/ChecklistFactory';
-
-import * as marker from './markers';
+import Maps from '../../components/Maps';
 
 import './styles.css';
 
-class Maps extends React.Component {
+class MapsView extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      layers: []
-    };
-
-    const { t, member } = this.props;
-
-    this.checklistFactory = new ChecklistFactory(t, member.data.profile, member.characterId, false);
+    this.state = {};
   }
 
   componentDidMount() {
     window.scrollTo(0, 0);
-
-    this.process();
   }
 
-  process = async () => {
-
-    let layers = map.map.layers;
-
-    layers = await Promise.all(
-      layers.map(async layer => {
-        return await fetch(layer.image)
-          .then(r => {
-            return r.blob();
-          })
-          .then(blob => {
-            const objectURL = URL.createObjectURL(blob);
-
-            layer.image = objectURL;
-            return layer;
-          })
-          .catch(e => {
-            console.log(e);
-          });
-      })
-    );
-
-    layers = await Promise.all(
-      layers.map(async layer => {
-        if (layer.color) {
-          const image = document.createElement('img');
-          image.src = layer.image;
-
-          await new Promise(resolve => {
-            image.onload = e => resolve();
-          });
-
-          const canvas = document.createElement('canvas');
-          canvas.width = layer.width;
-          canvas.height = layer.height;
-          const context = canvas.getContext('2d');
-
-          context.fillStyle = layer.color;
-          context.fillRect(0, 0, layer.width, layer.height);
-
-          context.globalCompositeOperation = 'destination-atop';
-          context.drawImage(image, 0, 0, layer.width, layer.height);
-
-          layer.image = canvas.toDataURL();
-
-          return layer;
-        } else {
-          return layer;
-        }
-      })
-    );
-
-    console.log(layers);
-
-    this.setState({ layers });
-  };
-
   render() {
-    const mapWidth = 2200;
-    const mapHeight = 2200;
-
-    const viewWidth = 1920;
-    const viewHeight = 1080;
-
-    const mapXOffset = (mapWidth - viewWidth) / 2;
-    const mapYOffset = -(mapHeight - viewHeight) / 2;
-
-    const bounds = [[0, 0], [mapHeight, mapWidth]];
-
-    console.log(map);
-
     return (
       <div className='view' id='maps'>
-        <div className='map'>
-          <div className='leaflet-pane leaflet-background-pane'>
-            {this.state.layers
-              .filter(layer => layer.type === 'background')
-              .map(layer => {
-                return <img key={layer.id} alt={layer.id} src={layer.image} className={cx('layer-background', `layer-${layer.id}`, { 'layer-interaction-none': true })} />;
-              })}
-          </div>
-          <Map center={[mapHeight / 2, mapWidth / 2]} zoom='0' minZoom='-3' maxZoom='1' maxBounds={bounds} crs={L.CRS.Simple} attributionControl={false}>
-            {this.state.layers
-              .filter(layer => layer.type !== 'background')
-              .map(layer => {
-                const layerScale = 1;
-                const layerX = layer.x ? layer.x : 0;
-                const layerY = layer.y ? -layer.y : 0;
-                const layerWidth = layer.width * layerScale;
-                const layerHeight = layer.height * layerScale;
-
-                let offsetX = (mapWidth - layerWidth) / 2;
-                let offsetY = (mapHeight - layerHeight) / 2;
-
-                offsetX += -offsetX + layerX + mapXOffset;
-                offsetY += offsetY + layerY + mapYOffset;
-
-                const bounds = [[offsetY, offsetX], [layerHeight + offsetY, layerWidth + offsetX]];
-
-                return <ImageOverlay key={layer.id} url={layer.image} bounds={bounds} />;
-              })}
-            {map.map.bubbles.map(bubble => bubble.nodes.filter(node => node.type === 'title').map((node, i) => {
-              console.log(node);
-
-              const markerOffsetX = mapXOffset+viewWidth/2;
-              const markerOffsetY = mapYOffset+mapHeight+-viewHeight/2;
-
-              const offsetX = markerOffsetX+(node.x ? node.x : 0);
-              const offsetY = markerOffsetY + (node.y ? node.y : 0);
-              
-              const icon = marker.textMarker(bubble.type, bubble.name);
-              
-              return <Marker key={i} position={[offsetY, offsetX]} icon={icon} />
-            }))}
-            {this.checklistFactory.ghostScans({ data: true }).checklist.items.filter(i => i.destinationHash === 2218917881).map(node => {
-              const markerOffsetX = mapXOffset+viewWidth/2;
-              const markerOffsetY = mapYOffset+mapHeight+-viewHeight/2;
-
-              const offsetX = markerOffsetX+(node.map.x ? node.map.x : 0);
-              const offsetY = markerOffsetY+(node.map.y ? node.map.y : 0);
-              
-              return <Marker key={node.itemHash} position={[offsetY, offsetX]} icon={marker.ghostScan} />
-            })}
-          </Map>
-        </div>
+        <Maps />
       </div>
     );
   }
@@ -173,18 +35,9 @@ function mapStateToProps(state, ownProps) {
   };
 }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    rebindTooltips: value => {
-      dispatch({ type: 'REBIND_TOOLTIPS', payload: new Date().getTime() });
-    }
-  };
-}
-
 export default compose(
   connect(
-    mapStateToProps,
-    mapDispatchToProps
+    mapStateToProps
   ),
   withTranslation()
-)(Maps);
+)(MapsView);
