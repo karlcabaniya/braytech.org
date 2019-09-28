eb79bc624766336f06e4ee421f2f444555487a0f
Tom Chapman <chapman@live.com.au>
2019-09-28 13:46:37 +1000

    Update getMember.js

--- src/utils/getMember.js
+++ src/utils/getMember.js
@@ -3,14 +3,22 @@ import * as bungie from './bungie';
 import * as responseUtils from './responseUtils';
 
 async function getMember(membershipType, membershipId) {
-  const auth = ls.get('setting.auth');
 
-  let components = [100,104,200,202,204,205,300,301,302,303,304,305,306,307,800,900];
-
+  const components = [100, 104, 200, 202, 204, 205, 300, 301, 302, 303, 304, 305, 306, 307, 800, 900, 1000];
   let useAuth = false;
-  if (auth && auth.destinyMemberships.find(m => m.membershipId === membershipId)) {
-    useAuth = true;
-    components.push(102,103,201);
+
+  try {
+    const tokens = ls.get('setting.auth');
+    const now = new Date().getTime() + 10000;
+
+    const tokenExpiry = tokens && new Date(tokens.access.expires).getTime();
+
+    if (tokens && tokens.destinyMemberships.find(m => m.membershipId === membershipId) && now < tokenExpiry) {
+      useAuth = true;
+      components.push(102,103,201);
+    }
+  } catch (e) {
+    console.log(e);
   }
 
   try {
