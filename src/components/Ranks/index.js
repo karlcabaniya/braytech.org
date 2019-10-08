3f6335a6acb6457135773ba64f2c64c16cfca996
Tom Chapman <chapman@live.com.au>
2019-10-09 04:30:42 +1000

    2.9.32

--- src/components/Ranks/index.js
+++ src/components/Ranks/index.js
@@ -41,169 +41,7 @@ class Mode extends React.Component {
         activityHash: 2947109551,
         iconClass: 'glory',
         totalPoints: utils.totalGlory(),
-        streakHash: 2572719399,
-        gains: {
-          '0': {
-            progressLoss: -60,
-            progressGain: {
-              '1': 80,
-              '2': 100,
-              '3': 120,
-              '4': 140,
-              '5': 160
-            }
-          },
-          '1': {
-            progressLoss: -60,
-            progressGain: {
-              '1': 80,
-              '2': 100,
-              '3': 120,
-              '4': 140,
-              '5': 160
-            }
-          },
-          '2': {
-            progressLoss: -60,
-            progressGain: {
-              '1': 80,
-              '2': 100,
-              '3': 120,
-              '4': 140,
-              '5': 160
-            }
-          },
-          '3': {
-            progressLoss: -60,
-            progressGain: {
-              '1': 68,
-              '2': 88,
-              '3': 108,
-              '4': 136,
-              '5': 148
-            }
-          },
-          '4': {
-            progressLoss: -60,
-            progressGain: {
-              '1': 68,
-              '2': 88,
-              '3': 108,
-              '4': 136,
-              '5': 148
-            }
-          },
-          '5': {
-            progressLoss: -60,
-            progressGain: {
-              '1': 68,
-              '2': 88,
-              '3': 108,
-              '4': 136,
-              '5': 148
-            }
-          },
-          '6': {
-            progressLoss: -52,
-            progressGain: {
-              '1': 60,
-              '2': 80,
-              '3': 100,
-              '4': 128,
-              '5': 140
-            }
-          },
-          '7': {
-            progressLoss: -52,
-            progressGain: {
-              '1': 60,
-              '2': 80,
-              '3': 100,
-              '4': 128,
-              '5': 140
-            }
-          },
-          '8': {
-            progressLoss: -52,
-            progressGain: {
-              '1': 60,
-              '2': 80,
-              '3': 100,
-              '4': 128,
-              '5': 140
-            }
-          },
-          '9': {
-            progressLoss: -60,
-            progressGain: {
-              '1': 40,
-              '2': 60,
-              '3': 80,
-              '4': 108,
-              '5': 120
-            }
-          },
-          '10': {
-            progressLoss: -60,
-            progressGain: {
-              '1': 40,
-              '2': 60,
-              '3': 80,
-              '4': 108,
-              '5': 120
-            }
-          },
-          '11': {
-            progressLoss: -60,
-            progressGain: {
-              '1': 40,
-              '2': 60,
-              '3': 80,
-              '4': 108,
-              '5': 120
-            }
-          },
-          '12': {
-            progressLoss: -68,
-            progressGain: {
-              '1': 40,
-              '2': 60,
-              '3': 80,
-              '4': 108,
-              '5': 120
-            }
-          },
-          '13': {
-            progressLoss: -68,
-            progressGain: {
-              '1': 40,
-              '2': 60,
-              '3': 80,
-              '4': 108,
-              '5': 120
-            }
-          },
-          '14': {
-            progressLoss: -68,
-            progressGain: {
-              '1': 40,
-              '2': 60,
-              '3': 80,
-              '4': 108,
-              '5': 120
-            }
-          },
-          '15': {
-            progressLoss: -68,
-            progressGain: {
-              '1': 40,
-              '2': 60,
-              '3': 80,
-              '4': 108,
-              '5': 120
-            }
-          }
-        }
+        streakHash: 2572719399
       }
     };
 
@@ -226,50 +64,6 @@ class Mode extends React.Component {
             }, 0)
           : '?'
     };
-  };
-
-  winsRequired = streakCount => {
-    const { characterId, characterProgressions } = this.props.data;
-
-    const debug = false;
-
-    let stepsData = this.data[2000925172].definition.steps;
-    let { stepIndex: currentStepIndex, stepIndex: workingStepIndex, progressToNextLevel: initialProgress, currentProgress } = characterProgressions[characterId].progressions[2000925172];
-    let targetStepIndex = currentStepIndex < 9 ? 9 : 15;
-
-    let winsRequired = 0;
-    let winsStreak = Math.max(streakCount, 1);
-    let totalProgress = currentProgress;
-    let progress = initialProgress;
-
-    if (debug) console.warn(`starting rank: ${stepsData[workingStepIndex].stepName} - ${currentProgress}`);
-
-    // just to Fabled I or go all the way to Legend+
-    while (targetStepIndex === 9 ? workingStepIndex < targetStepIndex : workingStepIndex <= targetStepIndex) {
-      const currentStep = stepsData[workingStepIndex];
-
-      if (debug) console.log(`stepIndex: ${workingStepIndex}, name: ${currentStep.stepName}`);
-
-      if (debug) console.log(`win: streak ${winsStreak}, progress ${progress}/${currentStep.progressTotal}`);
-      while (progress < currentStep.progressTotal) {
-        progress = progress + this.data[2000925172].gains[workingStepIndex].progressGain[Math.min(winsStreak, 5)];
-        totalProgress = totalProgress + this.data[2000925172].gains[workingStepIndex].progressGain[Math.min(winsStreak, 5)];
-
-        winsRequired++;
-
-        if (debug) console.log(`win: streak ${winsStreak}, progress ${progress}/${currentStep.progressTotal}`);
-      }
-
-      progress = Math.max(progress - currentStep.progressTotal, 0);
-
-      workingStepIndex++;
-    }
-
-    if (debug) console.warn(`finishing rank: ${stepsData[Math.min(workingStepIndex, stepsData.length - 1)].stepName} - ${totalProgress}`);
-
-    if (debug) console.log(winsRequired);
-
-    return winsRequired;
   };
 
   render() {
@@ -303,14 +97,14 @@ class Mode extends React.Component {
               <div className='value'>{characterProgressions[characterId].progressions[hash].currentProgress.toLocaleString('en-us')}</div>
             </div>
           ) : null}
-          {hash !== 2000925172 ? (
-            <>
-              {!mini ? (
+          {!mini ? (
                 <div>
                   <div className='name'>{t('Win streak')}</div>
                   <div className='value'>{characterProgressions[characterId].progressions[this.data[hash].streakHash].stepIndex}</div>
                 </div>
               ) : null}
+          {hash !== 2000925172 ? (
+            <>
               <div>
                 <div className='tooltip' data-hash='total_resets' data-table='BraytechDefinition'>
                   <div className='name'>{t('Total resets')}</div>
@@ -320,22 +114,6 @@ class Mode extends React.Component {
               <div>
                 <div className='name'>{t('Season resets')}</div>
                 <div className='value'>{this.data[hash].currentResetCount}</div>
-              </div>
-            </>
-          ) : null}
-          {hash === 2000925172 ? (
-            <>
-              <div>
-                <div className='name'>{t('Win streak')}</div>
-                <div className='value'>{characterProgressions[characterId].progressions[this.data[hash].streakHash].stepIndex}</div>
-              </div>
-              <div>
-                <div className='tooltip' data-hash='glory_wins_until' data-table='BraytechDefinition'>
-                  <div className='name'>{characterProgressions[characterId].progressions[2000925172].stepIndex < 9 ? t('Fabled rank') : t('Legend rank')}</div>
-                  <div className='value'>
-                    {this.winsRequired(characterProgressions[characterId].progressions[this.data[2000925172].streakHash].stepIndex)} {this.winsRequired(characterProgressions[characterId].progressions[this.data[2000925172].streakHash].stepIndex) ? (this.winsRequired(characterProgressions[characterId].progressions[this.data[2000925172].streakHash].stepIndex) === 1 ? t('win') : t('wins')) : '-'}
-                  </div>
-                </div>
               </div>
             </>
           ) : null}
